c0bafda04b5f22e7d39bb30ca6f0e79d7f7a6d99
takuyan <takuyan@Test-no-MacBook-Pro.local>
2012-09-28 15:20:32 +0900

    add gemrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "Delete the old files"
 rm ~/.vimrc
 rm ~/.zshrc
 rm ~/.gvimrc
+rm ~/.gemrc
 #rm ~/.gitconfig
 #rm ~/.gitignore
 
@@ -11,6 +12,7 @@ echo "Symlinking files"
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/gvimrc ~/.gvimrc
+ln -s ~/dotfiles/gemrc ~/.gemrc
 #ln -s ~/dotfiles/gitconfig ~/.gitconfig
 #ln -s ~/dotfiles/gitignore ~/.gitignore
 
